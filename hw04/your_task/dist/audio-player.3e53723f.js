b0c91106eae2080d2ef3750f72384fbab8a45ff8
Jihad Esmail <jae.esmail@gmail.com>
2021-05-26 20:27:19 -0500

    Checking in my hw04 folder

---
+++ hw04/your_task/dist/audio-player.3e53723f.js
@@ -0,0 +1,472 @@
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
+parcelRequire = (function (modules, cache, entry, globalName) {
+  // Save the require from previous bundle to this closure if any
+  var previousRequire = typeof parcelRequire === 'function' && parcelRequire;
+  var nodeRequire = typeof require === 'function' && require;
+
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire = typeof parcelRequire === 'function' && parcelRequire;
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
+        }
+
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
+        }
+
+        // Try the node require function if it exists.
+        if (nodeRequire && typeof name === 'string') {
+          return nodeRequire(name);
+        }
+
+        var err = new Error('Cannot find module \'' + name + '\'');
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
+      }
+
+      localRequire.resolve = resolve;
+      localRequire.cache = {};
+
+      var module = cache[name] = new newRequire.Module(name);
+
+      modules[name][0].call(module.exports, localRequire, module, module.exports, this);
+    }
+
+    return cache[name].exports;
+
+    function localRequire(x){
+      return newRequire(localRequire.resolve(x));
+    }
+
+    function resolve(x){
+      return modules[name][1][x] || x;
+    }
+  }
+
+  function Module(moduleName) {
+    this.id = moduleName;
+    this.bundle = newRequire;
+    this.exports = {};
+  }
+
+  newRequire.isParcelRequire = true;
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+  newRequire.register = function (id, exports) {
+    modules[id] = [function (require, module) {
+      module.exports = exports;
+    }, {}];
+  };
+
+  var error;
+  for (var i = 0; i < entry.length; i++) {
+    try {
+      newRequire(entry[i]);
+    } catch (e) {
+      // Save first error but execute all entries
+      if (!error) {
+        error = e;
+      }
+    }
+  }
+
+  if (entry.length) {
+    // Expose entry point to Node, AMD or browser globals
+    // Based on https://github.com/ForbesLindesay/umd/blob/master/template.js
+    var mainExports = newRequire(entry[entry.length - 1]);
+
+    // CommonJS
+    if (typeof exports === "object" && typeof module !== "undefined") {
+      module.exports = mainExports;
+
+    // RequireJS
+    } else if (typeof define === "function" && define.amd) {
+     define(function () {
+       return mainExports;
+     });
+
+    // <script>
+    } else if (globalName) {
+      this[globalName] = mainExports;
+    }
+  }
+
+  // Override the current require with this new one
+  parcelRequire = newRequire;
+
+  if (error) {
+    // throw error from earlier, _after updating parcelRequire_
+    throw error;
+  }
+
+  return newRequire;
+})({"js/audio-player.js":[function(require,module,exports) {
+var _this = this;
+
+var AudioPlayer = function AudioPlayer(selector, audioFile) {
+  var audio;
+  var playButton;
+  var rwdButton;
+  var ffwdButton;
+  var sliderInput;
+  var containerElement = document;
+
+  if (selector) {
+    containerElement = document.querySelector(selector);
+  }
+
+  _this.isPaused = function () {
+    return audio.paused;
+  };
+
+  _this.togglePlay = function () {
+    if (audio.paused) {
+      _this.play();
+    } else {
+      _this.pause();
+    }
+  };
+
+  _this.play = function () {
+    console.log('play');
+    playButton.classList.remove('fa-play-circle', 'fa-pause-circle');
+    jumpToTime();
+    audio.play();
+    playButton.classList.add('fa-pause-circle');
+  };
+
+  _this.pause = function () {
+    playButton.classList.remove('fa-play-circle', 'fa-pause-circle');
+    jumpToTime();
+    audio.pause();
+    playButton.classList.add('fa-play-circle');
+  };
+
+  var jumpToTime = function jumpToTime() {
+    var progressController = containerElement.querySelector('#player-time');
+    var timeElapsed = containerElement.querySelector('#time-elapsed');
+    var timeTotal = containerElement.querySelector('#time-total'); //console.log(getSliderPosition(), getCurrentTime(), getDuration());
+
+    if (getDuration() === "0NaN:0NaN") {
+      console.log('returning...');
+      return;
+    }
+
+    progressController.value = getSliderPosition();
+    timeElapsed.innerHTML = getCurrentTime();
+    timeTotal.innerHTML = getDuration();
+  };
+
+  var skipForward = function skipForward() {
+    if (audio.currentTime < audio.duration) {
+      audio.currentTime += audio.duration / 10;
+    } else {
+      audio.currentTime = audio.duration;
+    }
+  };
+
+  var skipBackward = function skipBackward() {
+    if (audio.currentTime > 0) {
+      audio.currentTime -= audio.duration / 10;
+    } else {
+      audio.currentTime = 0;
+    }
+  };
+
+  var customTime = function customTime(ev) {
+    var newTime = audio.duration * parseInt(ev.target.value) / 100;
+    audio.currentTime = parseInt(newTime) + 1;
+    jumpToTime();
+  };
+
+  var getSliderPosition = function getSliderPosition() {
+    return parseInt(audio.currentTime / audio.duration * 100);
+  };
+
+  var getDuration = function getDuration() {
+    return formatTime(audio.duration);
+  };
+
+  var getCurrentTime = function getCurrentTime() {
+    return formatTime(audio.currentTime);
+  };
+
+  var resetTime = function resetTime() {
+    audio.currentTime = 0;
+    playButton.classList.remove('fa-pause-circle');
+    playButton.classList.add('fa-play-circle');
+    jumpToTime();
+  };
+
+  var formatTime = function formatTime(timeCount) {
+    var seconds = timeCount,
+        minutes = Math.floor(seconds / 60);
+    minutes = minutes >= 10 ? minutes : "0" + minutes;
+    seconds = Math.floor(seconds % 60);
+    seconds = seconds >= 10 ? seconds : "0" + seconds;
+    return minutes + ":" + seconds;
+  };
+
+  _this.setAudioFile = function (audio_url) {
+    audio.src = audio_url;
+  };
+
+  _this.getAudioFile = function (audio_url) {
+    return audio.src;
+  };
+
+  _this.initialize = function () {
+    // init DOM hooks:
+    audio = containerElement.querySelector('#track');
+    playButton = containerElement.querySelector('#play_pause');
+    rwdButton = containerElement.querySelector('#rwd');
+    ffwdButton = containerElement.querySelector('#ffwd');
+    sliderInput = containerElement.querySelector('#player-time');
+    console.log(audio, playButton); // attach events to event handlers (i.e. functions)
+
+    audio.ontimeupdate = jumpToTime;
+    audio.onended = resetTime;
+
+    audio.onloadeddata = function () {
+      console.log("preview loaded"); //resetTime();
+      //this.pause();
+    };
+
+    playButton.onclick = _this.togglePlay;
+    rwdButton.onclick = skipBackward;
+    ffwdButton.onclick = skipForward;
+    sliderInput.oninput = customTime;
+    sliderInput.onchange = customTime;
+  };
+
+  _this.initialize();
+
+  if (audioFile) {
+    _this.setAudioFile(audioFile);
+  }
+
+  return _this;
+};
+},{}],"../../../.config/yarn/global/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+var global = arguments[3];
+var OVERLAY_ID = '__parcel__error__overlay__';
+var OldModule = module.bundle.Module;
+
+function Module(moduleName) {
+  OldModule.call(this, moduleName);
+  this.hot = {
+    data: module.bundle.hotData,
+    _acceptCallbacks: [],
+    _disposeCallbacks: [],
+    accept: function (fn) {
+      this._acceptCallbacks.push(fn || function () {});
+    },
+    dispose: function (fn) {
+      this._disposeCallbacks.push(fn);
+    }
+  };
+  module.bundle.hotData = null;
+}
+
+module.bundle.Module = Module;
+var checkedAssets, assetsToAccept;
+var parent = module.bundle.parent;
+
+if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
+  var hostname = "" || location.hostname;
+  var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62546" + '/');
+
+  ws.onmessage = function (event) {
+    checkedAssets = {};
+    assetsToAccept = [];
+    var data = JSON.parse(event.data);
+
+    if (data.type === 'update') {
+      var handled = false;
+      data.assets.forEach(function (asset) {
+        if (!asset.isNew) {
+          var didAccept = hmrAcceptCheck(global.parcelRequire, asset.id);
+
+          if (didAccept) {
+            handled = true;
+          }
+        }
+      }); // Enable HMR for CSS by default.
+
+      handled = handled || data.assets.every(function (asset) {
+        return asset.type === 'css' && asset.generated.js;
+      });
+
+      if (handled) {
+        console.clear();
+        data.assets.forEach(function (asset) {
+          hmrApply(global.parcelRequire, asset);
+        });
+        assetsToAccept.forEach(function (v) {
+          hmrAcceptRun(v[0], v[1]);
+        });
+      } else if (location.reload) {
+        // `location` global exists in a web worker context but lacks `.reload()` function.
+        location.reload();
+      }
+    }
+
+    if (data.type === 'reload') {
+      ws.close();
+
+      ws.onclose = function () {
+        location.reload();
+      };
+    }
+
+    if (data.type === 'error-resolved') {
+      console.log('[parcel] ✨ Error resolved');
+      removeErrorOverlay();
+    }
+
+    if (data.type === 'error') {
+      console.error('[parcel] 🚨  ' + data.error.message + '\n' + data.error.stack);
+      removeErrorOverlay();
+      var overlay = createErrorOverlay(data);
+      document.body.appendChild(overlay);
+    }
+  };
+}
+
+function removeErrorOverlay() {
+  var overlay = document.getElementById(OVERLAY_ID);
+
+  if (overlay) {
+    overlay.remove();
+  }
+}
+
+function createErrorOverlay(data) {
+  var overlay = document.createElement('div');
+  overlay.id = OVERLAY_ID; // html encode message and stack trace
+
+  var message = document.createElement('div');
+  var stackTrace = document.createElement('pre');
+  message.innerText = data.error.message;
+  stackTrace.innerText = data.error.stack;
+  overlay.innerHTML = '<div style="background: black; font-size: 16px; color: white; position: fixed; height: 100%; width: 100%; top: 0px; left: 0px; padding: 30px; opacity: 0.85; font-family: Menlo, Consolas, monospace; z-index: 9999;">' + '<span style="background: red; padding: 2px 4px; border-radius: 2px;">ERROR</span>' + '<span style="top: 2px; margin-left: 5px; position: relative;">🚨</span>' + '<div style="font-size: 18px; font-weight: bold; margin-top: 20px;">' + message.innerHTML + '</div>' + '<pre>' + stackTrace.innerHTML + '</pre>' + '</div>';
+  return overlay;
+}
+
+function getParents(bundle, id) {
+  var modules = bundle.modules;
+
+  if (!modules) {
+    return [];
+  }
+
+  var parents = [];
+  var k, d, dep;
+
+  for (k in modules) {
+    for (d in modules[k][1]) {
+      dep = modules[k][1][d];
+
+      if (dep === id || Array.isArray(dep) && dep[dep.length - 1] === id) {
+        parents.push(k);
+      }
+    }
+  }
+
+  if (bundle.parent) {
+    parents = parents.concat(getParents(bundle.parent, id));
+  }
+
+  return parents;
+}
+
+function hmrApply(bundle, asset) {
+  var modules = bundle.modules;
+
+  if (!modules) {
+    return;
+  }
+
+  if (modules[asset.id] || !bundle.parent) {
+    var fn = new Function('require', 'module', 'exports', asset.generated.js);
+    asset.isNew = !modules[asset.id];
+    modules[asset.id] = [fn, asset.deps];
+  } else if (bundle.parent) {
+    hmrApply(bundle.parent, asset);
+  }
+}
+
+function hmrAcceptCheck(bundle, id) {
+  var modules = bundle.modules;
+
+  if (!modules) {
+    return;
+  }
+
+  if (!modules[id] && bundle.parent) {
+    return hmrAcceptCheck(bundle.parent, id);
+  }
+
+  if (checkedAssets[id]) {
+    return;
+  }
+
+  checkedAssets[id] = true;
+  var cached = bundle.cache[id];
+  assetsToAccept.push([bundle, id]);
+
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    return true;
+  }
+
+  return getParents(global.parcelRequire, id).some(function (id) {
+    return hmrAcceptCheck(global.parcelRequire, id);
+  });
+}
+
+function hmrAcceptRun(bundle, id) {
+  var cached = bundle.cache[id];
+  bundle.hotData = {};
+
+  if (cached) {
+    cached.hot.data = bundle.hotData;
+  }
+
+  if (cached && cached.hot && cached.hot._disposeCallbacks.length) {
+    cached.hot._disposeCallbacks.forEach(function (cb) {
+      cb(bundle.hotData);
+    });
+  }
+
+  delete bundle.cache[id];
+  bundle(id);
+  cached = bundle.cache[id];
+
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    cached.hot._acceptCallbacks.forEach(function (cb) {
+      cb();
+    });
+
+    return true;
+  }
+}
+},{}]},{},["../../../.config/yarn/global/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","js/audio-player.js"], null)
+//# sourceMappingURL=/audio-player.3e53723f.js.map
--- hw04/your_task/js/scripts.js
+++ hw04/your_task/js/scripts.js
@@ -1,73 +1,128 @@
-const baseURL = 'https://www.apitutor.org/spotify/simple/v1/search';
+const baseURL = "https://www.apitutor.org/spotify/simple/v1/search";
 
 // Note: AudioPlayer is defined in audio-player.js
-const audioFile = 'https://p.scdn.co/mp3-preview/bfead324ff26bdd67bb793114f7ad3a7b328a48e?cid=9697a3a271d24deea38f8b7fbfa0e13c';
-const audioPlayer = AudioPlayer('.player', audioFile);
+const audioFile =
+  "https://p.scdn.co/mp3-preview/bfead324ff26bdd67bb793114f7ad3a7b328a48e?cid=9697a3a271d24deea38f8b7fbfa0e13c";
+const audioPlayer = AudioPlayer(".player", audioFile);
 
 const search = (ev) => {
-    const term = document.querySelector('#search').value;
-    console.log('search for:', term);
-    // issue three Spotify queries at once...
-    getTracks(term);
-    getAlbums(term);
-    getArtist(term);
-    if (ev) {
-        ev.preventDefault();
-    }
-}
+  const term = document.querySelector("#search").value;
+  console.log("search for:", term);
+  // issue three Spotify queries at once...
+  getTracks(term);
+  getAlbums(term);
+  getArtist(term);
+  if (ev) {
+    ev.preventDefault();
+  }
+};
 
 const getTracks = async (term) => {
-    const res = await fetch(`https://www.apitutor.org/spotify/simple/v1/search?type=track&q=${term}`)
-    const data = await res.json();
+  const res = await fetch(
+    `https://www.apitutor.org/spotify/simple/v1/search?type=track&q=${term}`
+  );
+  const data = await res.json();
 
-    const outputContainer = document.querySelector('#tracks')
+  const outputContainer = document.querySelector("#tracks");
 
-    if (data.length === 0) {
-        outputContainer.innerHTML = '<p>No tracks found</p>'
-        return;
-    }
+  if (data.length === 0) {
+    outputContainer.innerHTML = "<p>No tracks found</p>";
+    return;
+  }
 
+  const tracks = data.slice(0, 5);
 
-    console.log(data)
-
-    const tracks = data.slice(0, 5);
-
-    outputContainer.innerHTML = tracks.map(track => `
-        <section class="track-item preview" data-preview-track=${track.preview_url}>
+  outputContainer.innerHTML = tracks
+    .map(
+      (track) => `
+        <section class="track-item preview" data-preview-track="${
+          track.preview_url
+        }">
             <img src=${track.album.image_url}>
             <i class="fas play-track fa-play" aria-hidden="true"></i>
             <div class="label">
                 <h3>${track.name}</h3>
                 <p>
-                    ${track.artist.name}
+                    ${track.artist.name} ${
+        track.preview_url ? "" : "(no preview available)"
+      }
                 </p>
             </div>
         </section>
-    `).join('')
-    
+    `
+    )
+    .join("");
+
+  const tracksContainers = document.querySelectorAll(".track-item");
+
+  for (const trackContainer of tracksContainers) {
+    trackContainer.addEventListener("click", (e) => {
+      const previewTrack = e.currentTarget.dataset.previewTrack;
+      if (!previewTrack) {
+        return;
+      }
+
+      audioPlayer.setAudioFile(previewTrack);
+      audioPlayer.play();
+    });
+  }
 };
 
-const getAlbums = (term) => {
-    console.log(`
+const getAlbums = async (term) => {
+  console.log("HELKLKO");
+
+  console.log(`
         get albums from spotify based on the search term
         "${term}" and load them into the #albums section 
         of the DOM...`);
+
+  const res = await fetch(
+    `https://www.apitutor.org/spotify/simple/v1/search?type=album&q=${term}`
+  );
+  const albums = await res.json();
+
+  const outputContainer = document.querySelector("#albums");
+
+  if (albums.length === 0) {
+    outputContainer.innerHTML = "<p>No albums were returned.</p>";
+    return;
+  }
+
+  outputContainer.innerHTML = albums
+    .map(
+      (album) => `
+        <section class="album-card" id="2lATw9ZAVp7ILQcOKPCPqp">
+            <div>
+                <img src=${album.image_url}>
+                <h3>${album.name}</h3>
+                <div class="footer">
+                    <a href="${album.spotify_url}" target="_blank">
+                        view on spotify
+                    </a>
+                </div>
+            </div>
+        </section>
+    `
+    )
+    .join("");
 };
 
 const getArtist = async (term) => {
-    const res = await fetch(`https://www.apitutor.org/spotify/simple/v1/search?type=artist&q=${term}`)
-    const data = await res.json();
+  const res = await fetch(
+    `https://www.apitutor.org/spotify/simple/v1/search?type=artist&q=${term}`
+  );
+  const data = await res.json();
 
-    const outputContainer = document.querySelector('#artist')
+  const outputContainer = document.querySelector("#artist");
 
-    if (data.length === 0) {
-        outputContainer.innerHTML = '<p>No artists found</p>'
-        return;
-    }
+  if (data.length === 0) {
+    outputContainer.innerHTML = "<p>No artists found</p>";
+    return;
+  }
 
-    const artist = data[0];
+  const artist = data[0];
 
-    outputContainer.innerHTML = `
+  outputContainer.innerHTML = `
     <section class="artist-card" id=${artist.id}>
         <div>
             <img src=${artist.image_url}>
@@ -79,15 +134,14 @@ const getArtist = async (term) => {
             </div>
         </div>
     </section>
-    `
+    `;
 };
 
-
-document.querySelector('#search').onkeyup = (ev) => {
-    // Number 13 is the "Enter" key on the keyboard
-    console.log(ev.keyCode);
-    if (ev.keyCode === 13) {
-        ev.preventDefault();
-        search();
-    }
+document.querySelector("#search").onkeyup = (ev) => {
+  // Number 13 is the "Enter" key on the keyboard
+  console.log(ev.keyCode);
+  if (ev.keyCode === 13) {
+    ev.preventDefault();
+    search();
+  }
 };
